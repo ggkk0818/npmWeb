7f8b66d077b60f460f21defc13fa18dfecd5fa67
ggkk0818 <ggkk0818@sina.com>
2015-04-01 18:32:09 +0800

    调整首页拓扑图

--- TradeTracer/js/app/controllers/index.js
+++ TradeTracer/js/app/controllers/index.js
@@ -2,111 +2,32 @@
     "use strict";
     var module = angular.module('app.controllers');
     module.controller('IndexCtrl', function ($rootScope, $scope, $route, $interval, $location, $timeout, $window, logService, statisticService, warningService, warningSocketService) {
-        //变量
-        $scope.statisticPanel_TYPE = [
-            { name: "交易量", container: "index-chartDiv1" },
-            { name: "渠道", container: "index-chartDiv2" },
-            { name: "返回码", container: "index-chartDiv3" }
-        ];
-        $scope.dataPanel_MAX_SIZE = 50;
-        $scope.statisticPanel_current = null;
-        $scope.dataPanel_logList = null;
-        $scope.dataPanel_newLogCount = 0;
-        $scope.warnPanel_warnList = [];
         //告警时间线变量
         $scope.warnTimeLine_warnList = [];
         $scope.warnTimeLine_loading = true;
         $scope.warnTimeLine_animate = true;
-        $scope.warnTimeLine_interval = 5;
+        $scope.warnTimeLine_interval = 20;
         $scope.warnTimeLine_minSize = 30;
         $scope.warnTimeLine_maxSize = 50;
+        //拓扑图变量
+        $scope.topology_8583_count = 0;
+        $scope.topology_8583_duration = 0;
+        $scope.topology_8583_successRatio = 0;
+        $scope.topology_8583_warnCount = 0;
+        $scope.topology_20022_count = 0;
+        $scope.topology_20022_duration = 0;
+        $scope.topology_20022_successRatio = 0;
+        $scope.topology_20022_warnCount = 0;
+        $scope.topology_http_count = 0;
+        $scope.topology_http_duration = 0;
+        $scope.topology_http_successRatio = 0;
+        $scope.topology_http_warnCount = 0;
+        $scope.topology_mysql_count = 0;
+        $scope.topology_mysql_duration = 0;
+        $scope.topology_mysql_successRatio = 0;
+        $scope.topology_mysql_warnCount = 0;
         //初始化
         $scope.init = function () {
-            //数据表格
-            $scope.dataPanel_query();
-            //统计图
-            $timeout(function () {
-                $("#index-chartDiv1").trigger("configchange", [{
-                    plotOptions: {
-                        series: {
-                            color: "#f77d22",
-                            marker: { enabled: false }
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    }
-                }]);
-                //交易量
-                statisticService.list({ groupField: "statisticsTime", orderField: "statisticsTime", start: 0, limit: 10 }, function (data) {
-                    if (data && data.data) {
-                        var category = [], chartData = [];
-                        for (var i = 0; i < data.data.length; i++) {
-                            if (i >= 6)
-                                break;
-                            var time = new Date(data.data[i].group0),
-                                timeStr = time.Format("hh:mm");
-                            if (i + 1 == data.data.length)
-                                timeStr += "<br />" + time.Format("M月d日") + "<br />" + time.Format("yyyy年");
-                            category.push(timeStr);
-                            chartData.push(data.data[i].count);
-                        }
-                        $("#index-chartDiv1").trigger("chartupdate", [[category.reverse(), chartData.reverse()], ["时间", "交易量"]]);
-                    }
-                });
-                //返回码
-                statisticService.list({ groupField: "returnCode", orderField: "count", start: 0, limit: 10 }, function (data) {
-                    if (data && data.data) {
-                        var category = [], countData = [];
-                        for (var i = 0; i < data.data.length; i++) {
-                            if (i >= 6)
-                                break;
-                            category.push(data.data[i].group0);
-                            countData.push(data.data[i].count || 0);
-                        }
-                        if (data.data.length > 6) {
-                            category[5] = "Other";
-                            var otherCount = 0;
-                            for (var i = 5; i < data.data.length; i++) {
-                                otherCount += data.data[i].count || 0;
-                            }
-                            countData[5] = otherCount;
-                        }
-                        $("#index-chartDiv3").trigger("chartupdate", [[category, countData], ["返回码", "返回码"]]);
-                    }
-                });
-            });
-            //告警
-            if (!warningSocketService.isOpen())
-                warningSocketService.open();
-            warningSocketService.on("onmessage.indexWarnPanel", function (e) {
-                if (e && e.data) {
-                    var data = null;
-                    try {
-                        data = JSON.parse(e.data);
-                    }
-                    catch (err) { }
-                    if (data && data.warn && data.warn.length) {
-                        for (var i = 0; i < data.warn.length; i++) {
-                            var warn = data.warn[i],
-                                exists = false;
-                            for (var j = 0; j < $scope.warnPanel_warnList.length; j++) {
-                                if (warn.type === $scope.warnPanel_warnList[j].type) {
-                                    if (warn.count)
-                                        $scope.warnPanel_warnList[j].count = warn.count;
-                                    else
-                                        $scope.warnPanel_warnList.slice(j, 1);
-                                    exists = true;
-                                    break;
-                                }
-                            }
-                            if (!exists && warn.count)
-                                $scope.warnPanel_warnList.push(warn);
-                        }
-                    }
-                }
-            });
-            warningSocketService.query();
             //告警时间线
             for (var i = 0; i < $scope.warnTimeLine_minSize; i++) {
                 var block = { warnList: [] };
@@ -146,6 +67,8 @@
             warningService.list({ type: "resp20022", startTime: warnQueryStartTime, start: 0, limit: 0 }, function (data) { warnQueryDone(data, "resp20022"); });
             warningService.list({ type: "proc8583", startTime: warnQueryStartTime, start: 0, limit: 0 }, function (data) { warnQueryDone(data, "proc8583"); });
             warningService.list({ type: "proc20022", startTime: warnQueryStartTime, start: 0, limit: 0 }, function (data) { warnQueryDone(data, "proc20022"); });
+            if (!warningSocketService.isOpen())
+                warningSocketService.open();
             warningSocketService.on("onmessage.indexWarnTimeLine", function (e) {
                 if (e && e.data) {
                     var socketData = null;
@@ -207,176 +130,65 @@
             }
             $location.path("/warning").search(params);
         };
-        //查询实时数据
-        var dataPanelQueryTimer = null;
-        $scope.dataPanel_query = function () {
-            statisticService.list({ logType: 8583, start: 0, limit: 20 }, function (data) {
-                if (data && data.data) {
-                    //转换日期
-                    for (var i = 0; i < data.data.length; i++) {
-                        var record = data.data[i];
-                        if (typeof record.starttime_prefix === "number")
-                            record.starttime_prefix = new Date(record.starttime_prefix).Format("yyyy-MM-dd hh:mm:ss");
-                        if (typeof record.endtime_prefix === "number")
-                            record.endtime_prefix = new Date(record.endtime_prefix).Format("yyyy-MM-dd hh:mm:ss");
-                    }
-                    //比较数据新旧
-                    $scope.dataPanel_newLogCount = data.data.length || 0;
-                    if ($scope.dataPanel_logList && $scope.dataPanel_logList.length) {
-                        var lastId = $scope.dataPanel_logList[0].id,
-                            newLogCount = null;
-                        for (var i = 0; i < (data.data.length || 0); i++) {
-                            if (data.data[i].id === lastId) {
-                                newLogCount = i;
-                                break;
-                            }
-                        }
-                        if (typeof newLogCount === "number")
-                            $scope.dataPanel_newLogCount = newLogCount;
-                    }
-                    if ($scope.dataPanel_logList && $scope.dataPanel_newLogCount > 0) {
-                        for(var i = 0; i < $scope.dataPanel_newLogCount; i++){
-                            $scope.dataPanel_logList.splice(i, 0, data.data[i]);
-                        }
-                        if ($scope.dataPanel_logList.length > $scope.dataPanel_MAX_SIZE) {
-                            var c = $scope.dataPanel_logList.length - $scope.dataPanel_MAX_SIZE;
-                            $scope.dataPanel_logList.splice(-c, c);
-                        }
-                        $scope.dataPanel_showNewLog();
-                    } else {
-                        $scope.dataPanel_logList = data.data;
+        //查询拓扑图数据
+        var topologyTimer = null;
+        $scope.topology_query = function () {
+            statisticService.showTopology8583({ starttime: "2015-04-01 17:06:00", endtime: "2015-04-02 17:06:00" }, function (data) {
+                if (data && data.state == "200") {
+                    if (data.data && data.data.length) {
+                        var row = data.data[0];
+                        $scope.topology_8583_count = row.count;
+                        $scope.topology_8583_duration = row.avgDuration;
+                        $scope.topology_8583_successRatio = row.scount * 100 / row.count;
                     }
                 }
-                dataPanelQueryTimer = $timeout($scope.dataPanel_query, 5000);
             });
-        };
-        //告警点击跳转
-        $scope.warnPanel_query = function (warn) {
-            $location.path("/warning").search({ logType: warn.type });
-        };
-        //数据表格滚动效果
-        $scope.dataPanel_showNewLog = function () {
-            $timeout(function () {
-                var $dataTable = $("#dataPanelTable");
-                if ($dataTable.length) {
-                    if ($dataTable.find("tr").length == $scope.dataPanel_logList.length) {
-                        if ($dataTable.find("table").outerHeight() > $dataTable.height()) {
-                            var lineHeight = $dataTable.find("tr").first().outerHeight();
-                            $dataTable.animate({ scrollTop: lineHeight * $scope.dataPanel_newLogCount }, 0).animate({ scrollTop: 0 }, "slow", "easeInOutQuad");
-                        }
+            statisticService.showTopology20022({ starttime: "2015-04-01 17:06:00", endtime: "2015-04-02 17:06:00" }, function (data) {
+                if (data && data.state == "200") {
+                    if (data.data && data.data.length) {
+                        var row = data.data[0];
+                        $scope.topology_20022_count = row.count;
+                        $scope.topology_20022_duration = row.avgDuration;
+                        $scope.topology_20022_successRatio = row.scount * 100 / row.count;
                     }
-                    else {
-                        $scope.dataPanel_showNewLog();
+                }
+            });
+            statisticService.showTopologyHttp({ starttime: "2015-04-01 17:06:00", endtime: "2015-04-02 17:06:00" }, function (data) {
+                if (data && data.state == "200") {
+                    if (data.data && data.data.length) {
+                        var row = data.data[0];
+                        $scope.topology_http_count = row.count;
+                        $scope.topology_http_duration = row.avgDuration;
+                        $scope.topology_http_successRatio = row.scount * 100 / row.count;
+                    }
+                }
+            });
+            statisticService.showTopologyMysql({ starttime: "2015-04-01 17:06:00", endtime: "2015-04-02 17:06:00" }, function (data) {
+                if (data && data.state == "200") {
+                    if (data.data && data.data.length) {
+                        var row = data.data[0];
+                        $scope.topology_mysql_count = row.count;
+                        $scope.topology_mysql_duration = row.avgDuration;
+                        $scope.topology_mysql_successRatio = row.scount * 100 / row.count;
                     }
                 }
             });
         };
-        //统计Panel标签点击
-        $scope.changeStatisticType = function (t) {
-            $scope.statisticPanel_current = t;
-            //距离上次展示长宽有变，刷新图表
-            if ($scope.statisticPanel_current.isDirty) {
-                $timeout(function () {
-                    reloadChart($scope.statisticPanel_current);
-                }, 1000);
-            }
-        };
-        //图表重新初始化
-        var reloadChart = function (t) {
-            if (t && t.isDirty) {
-                $("#" + t.container).trigger("configchange", [{
-                    chart: {
-                        height: $("#statisticPanel .panel-body").height()
-                    }
-                }]);
-                t.isDirty = false;
-            }
-        };
+        $scope.topology_query();
         //窗口调整时更新各Panel高度
-        var windowResizeChartTimer = null;
         var windowResize = function () {
             var $dataPanel = $("#dataPanel"),
-                $warnPanel = $("#warnPanel"),
-                $statisticPanel = $("#statisticPanel"),
                 offsetTop = $dataPanel.parent().offset().top,
                 margin = 20;
             $dataPanel.outerHeight($($window).height() - offsetTop - margin);
-            $warnPanel.outerHeight(($($window).height() - offsetTop - margin * 2) / 2);
-            $statisticPanel.outerHeight(($($window).height() - offsetTop - margin * 2) / 2);
             //计算内容div高度
-            var $dataPanelTable = $("#dataPanelTable"),
-                $warnPanelBody = $("#warnPanelBody");
-            $dataPanelTable.height($dataPanel.height() - ($dataPanelTable.offset().top - offsetTop));
-            $warnPanelBody.outerHeight($warnPanel.height() - ($warnPanelBody.offset().top - offsetTop));
-            //调整统计图表高度
-            $("#statisticPanel .panel-body").outerHeight($warnPanel.height() - ($warnPanelBody.offset().top - offsetTop));
-            for (var i in $scope.statisticPanel_TYPE) {
-                $scope.statisticPanel_TYPE[i].isDirty = true;
-            }
-            $timeout.cancel(windowResizeChartTimer);
-            windowResizeChartTimer = $timeout(function () {
-                reloadChart($scope.statisticPanel_current);
-            }, 500);
+            var $indexTopology = $("#dataPanel .index_topology");
+            $indexTopology.outerHeight($dataPanel.height() - ($indexTopology.offset().top - offsetTop));
         };
         $($window).off("resize.index").on("resize.index", windowResize).trigger("resize.index");
-        //统计Panel自动切换
-        var remoteSolid = function () {
-            $("#statisticPanel .panel-heading .nav").find("li > a > .solid").remove();
-        };
-        var addSolid = function () {
-            $("#statisticPanel .panel-heading .nav > li > a").each(function (i, el) {
-                if ($(el).text() === $scope.statisticPanel_current.name) {
-                    $(el).append("<div class=\"solid\"></div>");
-                    $timeout.cancel(statisticPanelLoadingTimer);
-                    statisticPanelLoadingTimer = $timeout(function () {
-                        if ($(el).children(".solid").length) {
-                            $(el).children(".solid").css("width", "100%");
-                            statisticPanelTimer = $timeout(statisticPanelSwitch, 5000);
-                        }
-                    }, 1000);
-                    return false;
-                }
-            });
-        };
-        var statisticPanelSwitch = function () {
-            var i = $scope.statisticPanel_current ? $scope.statisticPanel_TYPE.indexOf($scope.statisticPanel_current) : $scope.statisticPanel_TYPE.length;
-            if (i + 1 >= $scope.statisticPanel_TYPE.length)
-                $scope.changeStatisticType($scope.statisticPanel_TYPE[0]);
-            else
-                $scope.changeStatisticType($scope.statisticPanel_TYPE[i + 1]);
-            remoteSolid();
-            addSolid();
-        };
-        var statisticPanelTimer = null;
-        var statisticPanelLoadingTimer = null;
-        $timeout(function () {
-            $("#statisticPanel").mouseenter(function () {
-                $scope.$apply(function () {
-                    remoteSolid();
-                    if (statisticPanelTimer) {
-                        $timeout.cancel(statisticPanelTimer);
-                        statisticPanelTimer = null;
-                    }
-                });
-            }).mouseleave(function () {
-                $scope.$apply(function () {
-                    if (statisticPanelTimer) {
-                        $timeout.cancel(statisticPanelTimer);
-                    }
-                    remoteSolid();
-                    addSolid();
-                });
-            });
-            statisticPanelSwitch();
-        });
         //离开该页事件
         $scope.$on("$routeChangeStart", function () {
             $($window).off("resize.index");
-            $timeout.cancel(dataPanelQueryTimer);
-            $timeout.cancel(windowResizeChartTimer);
-            $timeout.cancel(statisticPanelTimer);
-            $timeout.cancel(statisticPanelLoadingTimer);
-            warningSocketService.off("onmessage.indexWarnPanel");
             warningSocketService.off("onmessage.indexWarnTimeLine");
             $interval.cancel(warnTimeLineTimer);
         });
--- TradeTracer/js/app/services/statisticService.js
+++ TradeTracer/js/app/services/statisticService.js
@@ -23,5 +23,65 @@ function (angular, $, _, config) {
                     callback(null);
             });
         };
+
+        this.showTopology8583 = function (params, callback) {
+            $http({
+                method: 'GET',
+                url: 'statistics/8583/show',
+                params: params,
+                cache: false
+            }).success(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(data);
+            }).error(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(null);
+            });
+        };
+
+        this.showTopology20022 = function (params, callback) {
+            $http({
+                method: 'GET',
+                url: 'statistics/20022/show',
+                params: params,
+                cache: false
+            }).success(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(data);
+            }).error(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(null);
+            });
+        };
+
+        this.showTopologyHttp = function (params, callback) {
+            $http({
+                method: 'GET',
+                url: 'statistics/http/show',
+                params: params,
+                cache: false
+            }).success(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(data);
+            }).error(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(null);
+            });
+        };
+
+        this.showTopologyMysql = function (params, callback) {
+            $http({
+                method: 'GET',
+                url: 'statistics/mysql/show',
+                params: params,
+                cache: false
+            }).success(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(data);
+            }).error(function (data, status, headers, config) {
+                if (typeof callback === "function")
+                    callback(null);
+            });
+        };
     });
 });
